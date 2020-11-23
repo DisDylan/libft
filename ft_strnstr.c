b5e6b86b1c5add6914a07b6ef334fbd58d354201
dpoinsu <dpoinsu@student.42.fr>
2020-11-23 14:25:24 +0100

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 15:22:45 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/11/21 12:04:28 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/11/23 14:24:51 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,8 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (!big || !little)
-		return (NULL);
 	if (ft_strlen(little) > ft_strlen(big))
-		return ((char*)big);
+		return (NULL);
 	while (big[i] && i < len)
 	{
 		j = 0;
