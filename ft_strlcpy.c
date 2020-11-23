976cb151dc770f9af25335e6893dba786d7471b3
dpoinsu <dpoinsu@student.42.fr>
2020-11-23 14:42:25 +0100

    push

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 14:59:42 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/11/23 14:40:07 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/11/23 14:41:56 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
-	if (!src || size == 0)
+	if (!src)
 		return (0);
 	i = 0;
 	if (size > 0)
