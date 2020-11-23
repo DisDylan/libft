2e1c026bfadea6ff09883eb782e317374bc8e854
dpoinsu <dpoinsu@student.42.fr>
2020-11-23 16:09:01 +0100

    ok

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 15:33:58 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/11/18 11:01:56 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/11/23 16:08:56 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,8 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void *str;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	if (!(str = (void *)malloc(nmemb * size)))
+	str = (void*)malloc(count*size);
+	if (!str)
 		return (NULL);
 	ft_bzero(str, nmemb);
 	return (str);
