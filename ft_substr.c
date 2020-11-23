19373c637bc0405b6e539a243e65ed23ce70d3e3
Dylan POINSU <dpoinsu@e3r11p9.42.fr>
2020-11-23 16:54:10 +0100

    okok

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 14:30:41 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/11/23 16:24:08 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/11/23 16:50:57 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	j;
 	char	*str;
 
-	if (!(str = (char *)malloc(sizeof(s) * (len))))
+	if ((start == 0 && len ==0) || !s)
+		return (NULL);
+	if (!(str = (char *)malloc(sizeof(*s) * (len + 1))))
 		return (NULL);
 	i = 0;
 	j = 0;
