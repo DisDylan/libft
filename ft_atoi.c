2343f3ac4c1cc25caaa9fb534ff641913454292d
dpoinsu <dpoinsu@student.42.fr>
2020-11-23 16:04:33 +0100

    ok

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 15:32:38 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/11/23 14:54:16 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/11/23 14:58:46 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,18 @@ int	ft_atoi(const char *str)
 	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' ||
 			str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
 		i++;
-	if (str[i++] == '-')
+	if (str[i] == '-')
+	{
 		neg *= -1;
+		i++;
+	}
 	else if (str[i] == '+')
 		i++;
-	while (str[i++] >= '0' && str[i] <= '9')
+	while (str[i] >= '0' && str[i] <= '9')
+	{
 		num = num * 10 + (str[i] - 48);
+		i++;
+	}
 	num *= neg;
-	if (num > 2147483647)
-		return (-1);
-	if (num < -2147483648)
-		return (0);
 	return (num);
 }
