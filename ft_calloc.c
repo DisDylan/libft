3221d2e1095c64ca9d2a3521139a70b59d2b87e7
Dylan POINSU <dpoinsu@e3r11p9.42.fr>
2020-11-23 16:59:26 +0100

    ok

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: dpoinsu <dpoinsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 15:33:58 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/11/23 16:14:04 by dpoinsu          ###   ########.fr       */
+/*   Updated: 2020/11/23 16:55:49 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,10 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	void *str;
+	char	*new;
 
-	str = (void*)malloc(nmemb*size);
-	if (!str)
+	if (!(new = malloc(size * nmemb)))
 		return (NULL);
-	if (nmemb == 0 || size == 0)
-	{
-		str = (void*)malloc(sizeof(str));
-		return (str);
-	}
-	ft_bzero(str, nmemb);
-	return (str);
+	ft_memset(new, 0, nmemb * size);
+	return (new);
 }
